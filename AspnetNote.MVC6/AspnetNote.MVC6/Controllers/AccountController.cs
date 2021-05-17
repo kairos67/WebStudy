61a2c8d5f44e346717393e7495f654e758431d48
kairos67 <kairosys@gmail.com>
2021-05-17 03:22:07 -0700

    add AspnetNote6 from home

---
+++ AspnetNote.MVC6/AspnetNote.MVC6/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.MVC6.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// login
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return View();
+        }
+        /// <summary>
+        /// register
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register() 
+        {
+            return View();
+        }
+    }
+}
