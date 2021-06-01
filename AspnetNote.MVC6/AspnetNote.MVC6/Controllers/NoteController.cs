a853c0a5c76aaef8ce01a2da7c5c7cafab334f51
kairos67 <kairosys@gmail.com>
2021-06-01 19:54:37 +0900

    added note

---
+++ AspnetNote.MVC6/AspnetNote.MVC6/Controllers/NoteController.cs
@@ -0,0 +1,35 @@
+﻿using AspnetNote.MVC6.DataContext;
+using AspnetNote.MVC6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.MVC6.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new AspnetNoteDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+            
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
