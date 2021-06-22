e2bd5d86bd2aa12c871af4c0d5e7c0ca506c532d
kairos67 <kairosys@gmail.com>
2021-06-22 19:49:57 +0900

    add Enterprise Architecture

---
+++ AspnetNote.MVC6/Note.BLL/UserBll.cs
@@ -0,0 +1,27 @@
+﻿using Note.IDAL;
+using Note.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Note.BLL
+{
+    public class UserBll 
+    {
+        //private UserDal _userDal = new UserDal(); //X
+        private readonly IUserDal _userDal; //O
+
+        public UserBll(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+        public User GetUser(int userNo)
+        {
+            return _userDal.GetUser(userNo);
+        }
+
+        public List<User> GetUserList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
